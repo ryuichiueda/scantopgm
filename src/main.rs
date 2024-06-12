e3a2163472e86b81d2161ab5f4452b9fa4ab120f
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-12 13:28:57 +0900

    Add a license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+//SPDX-FileCopyrightText: 2024 Ryuichi Ueda ryuichiueda@gmail.com
+//SPDX-License-Identifier: BSD-3-Clause
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    let len = io::stdin()
+              .read_line(&mut line)
+               .expect("scantopgm: Failed to read line");
+
+    eprintln!("{:?}", &line);
 }
