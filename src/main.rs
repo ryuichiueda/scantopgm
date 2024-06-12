2dd948a70292118d48f2f43f55259c75062e0c62
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-12 15:13:50 +0900

    Enable to use a crate

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,39 @@
 //SPDX-License-Identifier: BSD-3-Clause
 
 use std::io;
+use yaml_rust::YamlLoader;
+use yaml_rust::Yaml::Integer;
 
-fn header_to_filename() -> Option<String> {
-    let mut sec = String::new();
-
+fn read_yaml(text: &mut String) {
     loop {
         let mut line = String::new();
 
         if io::stdin().read_line(&mut line)
-           .expect("scantopgm: Failed to read line") == 0 {
+           .expect("scantopgm: Failed to read line") == 0 
+        || line.starts_with("---") {
             break;
         }
 
-        let line = line.trim().to_string();
-        let name = line.split(":").nth(0).unwrap_or("");
-        let data = line.split(":").nth(1).unwrap_or("");
-
-        match name {
-            "header" | "stamp" => {}, 
-            "sec" => sec = data.to_string(),
-            "nanosec" => {
-                let ans = format!("{}.{:>09}", sec, data.trim()).replace(" ", "0");
-                return Some(ans);
-            },
-            _ => {},
-        }
+        *text += &line;
     }
-
-    None
 }
 
 fn main() {
-    loop {
-        let filename = header_to_filename();
-        if filename == None {
-            return;
-        }
-        println!("{:?}", filename);
-    }
+    let mut text = String::new();
+    read_yaml(&mut text);
+    let data = YamlLoader::load_from_str(&text).unwrap();
+    let header = &data[0]["header"];
+
+    let sec = match header["stamp"]["sec"] {
+        Integer(n) => n,
+        _ => panic!("scantopgm: No timestamp"),
+    };
+
+    let nsec = match header["stamp"]["nanosec"] {
+        Integer(n) => n,
+        _ => panic!("scantopgm: No timestamp"),
+    };
+
+    let filename = format!("{}.{:09}", &sec, &nsec);
+    dbg!("{:?}", &filename);
 }
