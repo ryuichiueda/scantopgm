d17fed67966060825b4c6023c21b370f37286828
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-06-12 14:25:42 +0900

    Make filename

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,41 @@
 
 use std::io;
 
-fn main() {
+fn header_to_filename() -> Option<String> {
+    let mut sec = String::new();
+
     loop {
         let mut line = String::new();
-        let len = io::stdin()
-                .read_line(&mut line)
-                .expect("scantopgm: Failed to read line");
-    
-        if len == 0 {
+
+        if io::stdin().read_line(&mut line)
+           .expect("scantopgm: Failed to read line") == 0 {
             break;
         }
-        eprintln!("{:?}", &line);
+
+        let line = line.trim().to_string();
+        let name = line.split(":").nth(0).unwrap_or("");
+        let data = line.split(":").nth(1).unwrap_or("");
+
+        match name {
+            "header" | "stamp" => {}, 
+            "sec" => sec = data.to_string(),
+            "nanosec" => {
+                let ans = format!("{}.{:>09}", sec, data.trim()).replace(" ", "0");
+                return Some(ans);
+            },
+            _ => {},
+        }
+    }
+
+    None
+}
+
+fn main() {
+    loop {
+        let filename = header_to_filename();
+        if filename == None {
+            return;
+        }
+        println!("{:?}", filename);
     }
 }
